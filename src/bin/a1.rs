20b7a97394c222b08f0e60837c22e3d592b15047
limaleonardo <lima.leonardo.r@gmail.com>
2024-02-03 10:28:18 -0300

    include activities files and completes activity 1

---
+++ src/bin/a1.rs
@@ -0,0 +1,22 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+
+fn display_first_name() {
+    println!("My first name is: Léo");
+}
+
+fn display_last_name() {
+    println!("My last name is: Lima");
+}
+fn main() {
+    display_first_name();
+    display_last_name();
+}
