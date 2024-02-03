2389d4fc45b9c629f88e91d0a77ea46f843dc0bb
limaleonardo <lima.leonardo.r@gmail.com>
2024-02-03 10:36:50 -0300

    finaliza atividades basicas

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,16 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add_two_numbers(a:i32, b:i32) -> i32 {
+    a + b
+}
+
+fn display_result(result:i32) {
+    println!("The result is: {:?}", result);
+}
+fn main() {
+    let a = 10;
+    let b = 20;
+    let result = add_two_numbers(a, b);
+    display_result(result);
+}
